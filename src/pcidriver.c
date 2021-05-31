211d09edaa15de14913b7db5a0927f0ec6095d92
Peter Nerlich <PeterNerlich@users.noreply.github.com>
2021-05-31 07:42:28 +0200

    some documentation

--- src/pcidriver.c
+++ src/pcidriver.c
@@ -20,21 +20,24 @@ static struct class *mypci_class;
 static struct device *mypci_dev;
 
 static int driver_open( struct inode *devfile, struct file *instance )
-{
+{	// 'open' operation for the device file
+	// we don't really have anything to do here
 	printk(KERN_INFO "mypci driver_open\n");
 	//dev_info( mypci_dev, "driver_open called\n" );
 	return 0;
 }
 
 static int driver_close( struct inode *devfile, struct file *instance )
-{
+{	// 'close' operation for the device file
+	// we don't have anything to do here either
 	printk(KERN_INFO "mypci driver_close\n");
 	//dev_info( mypci_dev, "driver_close called\n" );
 	return 0;
 }
 
 static ssize_t driver_read( struct file *instance, char __user *user, size_t count, loff_t *offset )
-{
+{	// 'read' operation for the device file
+	// we want this to trigger the measurement and give the result
 	printk(KERN_INFO "mypci driver_read\n");
 
 	u8 status, timeout, length;
@@ -43,19 +46,24 @@ static ssize_t driver_read( struct file *instance, char __user *user, size_t cou
 	char outs[32];	// buffer for formatting output to ascii
 
 	for (timeout = 0; timeout < 25; timeout++)
-	{
+	{	// waiting for the converter to become available
+		// try a max of 25 times, else error out
 		status = inb(ioport+0x80);	// read a/d status again
 		if (status & 0x80 > 0) break;	// stop if not busy
 		usleep_range(5000, 10000);	// sleep for a minimum of 5 and maximum of 10 milliseconds
+		// we use 'usleep_range' so we don't block the whole kernel
+		// usleep ensures we are woken up at least at the last duration specified
+		// but we might be called as early as the first duration specified if there's already another convenient interrupt going on
 	}
 	if (status & 0x80 == 0)
-	{	// if we left the loop because of timeout
+	{	// if we left the loop not normally but because of timeout
 		raw_copy_to_user(user, "[TIMEOUT] waiting for device (busy)\n", 37);
 		return 37;	// that is the length of the string plus the null byte
 	}
 	
 	for (timeout = 0; timeout < 25; timeout++)
-	{
+	{	// waiting for the converter to become empty
+		// try a max of 25 times, else error out
 		status = inb(ioport+0x80);	// read a/d status again
 		if (status & 0x10 > 0) break;	// stop if empty
 		
@@ -70,7 +78,8 @@ static ssize_t driver_read( struct file *instance, char __user *user, size_t cou
 	outb(0xff, ioport+0x0e);	// trigger conversion (value can be anything, but register must be written)
 
 	for (timeout = 0; timeout < 25; timeout++)
-	{
+	{	// waiting for the converter to become available
+		// try a max of 25 times, else error out
 		status = inb(ioport+0x80);	// read a/d status again
 		if (status & 0x80 > 0) break;	// stop if not busy
 		usleep_range(5000, 10000);	// sleep for a minimum of 5 and maximum of 10 milliseconds
@@ -88,20 +97,23 @@ static ssize_t driver_read( struct file *instance, char __user *user, size_t cou
 		K    = 2047×16 = 32752
 		gain = 1		*/
 	/* if we have 64 bit to do the calculation, and our dividend is only 16 bit, we can use the rest to get more precision!
-	the above requires us to multiply data by ten, we should be able to do that 13 more times without risking an overflow. that means 13 decimal places for free! with no floating point magic whatsoever!
+	the above requires us to multiply data by ten, we should be able to do that 13 more times without risking an overflow.
+	that means 13 decimal places for free! with no floating point magic whatsoever! yaay!
 	*/
-	volt = div_s64(data * 10 * pow(10, 13), 32752);
+	volt = div_s64(data * 10 * pow(10, 13), 32752);	// kinda adjusted formula: data boosted to 13 extra decimal places, devision only last step
 	length = snprintf(outs, 32, "%lld", volt);	// format the read number to a string
+	// wait a minute, what about the decimal point?
 	for (int i = length; i > length-13; --i)
 	{	// shift 14 digits one place later
 		outs[i] = outs[i-1];
 	}
 	outs[length-13] = '.';	// insert the dot
 	length = snprintf(outs, 32, "%s V\n", outs);	// typeset the whole to a string of max length of 32 (including null character)
-	raw_copy_to_user(user, &outs, length+1);
+	raw_copy_to_user(user, &outs, length+1);	// and out with it to userspace!
 	return length+1;
 }
 
+// we declare which functions handle which file operations
 static struct file_operations fops = {
 	.owner = THIS_MODULE,
 	.read = driver_read,
@@ -110,15 +122,15 @@ static struct file_operations fops = {
 };
 
 static int device_init( struct pci_dev *pdev, const struct pci_device_id *id )
-{
+{	// when the kernel found a device to use this driver for
 	printk(KERN_INFO "mypci device_init\n");
 
 	if( pci_enable_device( pdev ) )
 		return -EIO;
-	ioport = pci_resource_start( pdev, 2 );
-	iolen = pci_resource_len( pdev, 2 );
+	ioport = pci_resource_start( pdev, 2 );	// get the address of the memory region
+	iolen = pci_resource_len( pdev, 2 );	// ...and the length!
 	if( request_region( ioport, iolen, pdev->dev.kobj.name )==NULL )
-	{
+	{	// uh oh! we requested the region to use but something went wrong.
 		dev_err(&pdev->dev,"I/O address conflict for device \"%s\"\n",
 			pdev->dev.kobj.name);
 		return -EIO;
@@ -127,17 +139,18 @@ static int device_init( struct pci_dev *pdev, const struct pci_device_id *id )
 	pci_write_config_byte(pdev, ioport+0x08, 0x0);  // set signal range to ±10V
 	pci_write_config_byte(pdev, ioport+0x0A, 0x0);  // set trigger to software/polling
 
+	// finally, create the device file that userspace can interact with
 	mypci_dev = device_create( mypci_class, NULL, mypci_dev_number, NULL, "%s", "mypci" );
 	return 0;
 }
 
 static void device_deinit( struct pci_dev *pdev )
-{
+{	// when the device disappeares
 	printk(KERN_INFO "mypci device_deinit\n");
 
-	device_destroy( mypci_class, mypci_dev_number );  // remove the device file
+	device_destroy( mypci_class, mypci_dev_number );	// remove the device file
 	if( ioport )
-		release_region( ioport, iolen );
+		release_region( ioport, iolen );	// release the memory region
 }
 
 static struct file_operations fops;
@@ -155,7 +168,7 @@ static struct pci_driver pci_drv = {
 };
 
 static int __init mod_init(void)
-{
+{	// when the module is loaded
 	printk(KERN_INFO "mypci mod_init\n");
 
 	if( alloc_chrdev_region(&mypci_dev_number, 0, 1, "mypci") < 0 )
@@ -188,7 +201,7 @@ free_dev_number:
 }
 
 static void __exit mod_exit(void)
-{
+{	// when the module is unloaded
 	printk(KERN_INFO "mypci mod_exit\n");
 
 	pci_unregister_driver( &pci_drv );
